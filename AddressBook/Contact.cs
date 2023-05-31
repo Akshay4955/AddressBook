f7a128f47bb23c3b93c8c6f3320d17a5e294e2d5
Akshay4955 <akshayshedge11@gmail.com>
2023-05-31 20:04:22 +0530

    Added: Code to create contact

---
+++ AddressBook/Contact.cs
@@ -0,0 +1,21 @@
+﻿namespace AddressBook;
+
+public class Contact
+{
+    public string firstName;
+    public string lastName;
+    public string address;
+    public string city;
+    public string state;
+    public string zipcode;
+    public string phoneNumber;
+    public string email;
+
+    public override string ToString()
+    {
+        return firstName.ToString() + " " + lastName.ToString() + " " + address.ToString() + " " +
+            address.ToString() + " " + city.ToString() + " " + state.ToString() + " " + 
+            zipcode.ToString() + " " + phoneNumber.ToString() + " " + email.ToString();
+    }
+
+}
--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace AddressBook
+﻿namespace AddressBook;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Welcome to addressbook project...!!!");
-        }
+        Console.WriteLine("Welcome to addressbook project...!!!");
+        Console.WriteLine("Plz enter contact details");
+        Contact contact = new ();
+        Console.WriteLine("Plz enter first name of contact");
+        contact.firstName =  Console.ReadLine();
+        Console.WriteLine("Plz enter last name of contact");
+        contact.lastName = Console.ReadLine();
+        Console.WriteLine("Plz enter address of contact");
+        contact.address = Console.ReadLine();
+        Console.WriteLine("Plz enter city of contact");
+        contact.city = Console.ReadLine();
+        Console.WriteLine("Plz enter state of contact");
+        contact.state = Console.ReadLine();
+        Console.WriteLine("Plz enter zipCode of contact");
+        contact.zipcode = Console.ReadLine();
+        Console.WriteLine("Plz enter phoneNumber of contact");
+        contact.phoneNumber = Console.ReadLine();
+        Console.WriteLine("Plz enter email of contact");
+        contact.email = Console.ReadLine();
+        Console.WriteLine(contact.ToString());
     }
 }
